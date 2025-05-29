d4536e5ba1f3a49e2244be84e193a544644f0737
Kagramanov Artem Nikolaevich <AKagramanov@gksm.local>
2025-05-29 21:51:00 +0200

    Add backend/frontend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,41 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OffersModule } from './offers/offers.module';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { User } from './users/entities/user.entity';
+import { Wish } from './wishes/entities/wish.entity';
+import { Wishlist } from './wishlists/entities/wishlist.entity';
+import { Offer } from './offers/entities/offer.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from './auth/auth.module';
+
+const configService = new ConfigService();
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: configService.get('POSTGRES_HOST'),
+      port: configService.get('POSTGRES_PORT'),
+      username: configService.get('POSTGRES_USERNAME'),
+      password: configService.get('POSTGRES_PASSWORD'),
+      database: configService.get('POSTGRES_DB'),
+      entities: [User, Offer, Wish, Wishlist],
+      synchronize: configService.get('POSTGRES_SYNCHRONIZE'),
+    }),
+    TypeOrmModule.forFeature([User, Wish, Wishlist, Offer]),
+    OffersModule,
+    UsersModule,
+    WishesModule,
+    WishlistsModule,
+    JwtModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
